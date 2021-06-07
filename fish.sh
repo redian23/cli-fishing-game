e2c451fbfb74e493b60703cc079501f647e70ee5
Redian23 <vasya.degski@gmail.com>
2021-06-07 10:20:11 +0300

    Upload 0.2.X Beta

--- fish.sh
+++ fish.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ANIMAT1="\n
+#TODO
+# раскрасить ехо
+# картинки рыб
+# 
+# 
+#
+
+#SPREY'S
+
+WAITING_ANIMATION_POSITION1_1="\n
 _________________________\n
 |\t\t\t			        |\n 
 |\t\t\t			        |\n
@@ -14,7 +23,7 @@ _________________________\n
 |\t\t\t			        |\n
 |_______________________|\n 
 "
-ANIMAT2="\n
+WAITING_ANIMATION_POSITION1_2="\n
 _________________________\n
 |\t\t\t                 |\n
 |\t\t\t                 |\n
@@ -29,7 +38,7 @@ _________________________\n
 |_______________________|\n
 "
 
-ANIMAT3="\n
+WAITING_ANIMATION_POSITION1_3="\n
 _________________________\n
 |\t\t\t                 |\n
 |\t\t\t                 |\n
@@ -44,7 +53,7 @@ _________________________\n
 |_______________________|\n
 "
 
-ANIMAT10="\n
+BITE_ANIMATION_POSITION1_1="\n
 _________________________\n
 |\t\t\t                 |\n
 |\t\t\t                 |\n
@@ -60,7 +69,7 @@ _________________________\n
 "
 
 
-ANIMAT11="\n
+BITE_ANIMATION_POSITION1_2="\n
 _________________________\n
 |\t\t\t                 |\n
 |\t\t\t                 |\n
@@ -75,7 +84,7 @@ _________________________\n
 |_______________________|\n
 "
 
-ANIMAT12="\n
+BITE_ANIMATION_POSITION1_3="\n
 _________________________\n
 |\t\t\t                 |\n
 |\t\t\t                 |\n
@@ -90,104 +99,500 @@ _________________________\n
 |_______________________|\n
 "
 
-#Start Script
+##/-----------------------------------/
 
+WAITING_ANIMATION_POSITION2_1="\n
+_________________________\n
+|\t\t\t			        |\n 
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t  \  \t\t		    |\n
+|\t  o  \t\t 		    |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|_______________________|\n 
+"
+WAITING_ANIMATION_POSITION2_2="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t  |  \t\t            |\n
+|\t  o  \t\t            |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+WAITING_ANIMATION_POSITION2_3="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t  /  \t\t            |\n
+|\t  o  \t\t            |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+BITE_ANIMATION_POSITION2_1="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t  (o)  \t\t          |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+
+BITE_ANIMATION_POSITION2_2="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t( o ) \t\t           |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+BITE_ANIMATION_POSITION2_3="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t (   o   )\t\t       |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+##/-----------------------------------/
+
+WAITING_ANIMATION_POSITION3_1="\n
+_________________________\n
+|\t\t\t			        |\n 
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t    \  \t		    |\n
+|\t\t    o  \t		    |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|\t\t\t			        |\n
+|_______________________|\n 
+"
+WAITING_ANIMATION_POSITION3_2="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t    |  \t          |\n
+|\t\t    o  \t          |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+WAITING_ANIMATION_POSITION3_3="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t    /  \t          |\n 
+|\t\t    o  \t          |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+BITE_ANIMATION_POSITION3_1="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t    (o)   \t       |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+
+BITE_ANIMATION_POSITION3_2="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t   ( o )   \t      |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+BITE_ANIMATION_POSITION3_3="\n
+_________________________\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t   (   o   ) \t    |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|\t\t\t                 |\n
+|_______________________|\n
+"
+
+
+#Welcome
 clear
-echo "Welcome!" 
+echo -e "\e[32mWelcome!\e[0m" 
 echo "You are start to play in Console Fishing"
 echo "This is simlpe bash script, but to have so mach fun."
-echo "Version 0.0.7 pre-alpha" 
+echo -e "\e[33mCreate by Redian23\e[0m"
+echo -e "\e[31mVersion 0.2.12 beta\e[0m"
 sleep 5s
 
-# Select one of many position poplowka
+#START GAME 
+
+select_position(){
+
+# Select one of many position of bobber
+clear
+echo "Please enter position your bobber:"
+positions=("Positions 1" "Positions 2" "Positions 3" "Quit")
+select opt in "${positions[@]}"
+do
+    case $opt in
+        "Positions 1")
+            echo "you chose position 1"
+            WAITING_ANIMATION_1=$WAITING_ANIMATION_POSITION1_1
+            WAITING_ANIMATION_2=$WAITING_ANIMATION_POSITION1_2
+            WAITING_ANIMATION_3=$WAITING_ANIMATION_POSITION1_3
+            BITE_ANIMATION_1=$BITE_ANIMATION_POSITION1_1
+            BITE_ANIMATION_2=$BITE_ANIMATION_POSITION1_2
+            BITE_ANIMATION_3=$BITE_ANIMATION_POSITION1_3
+            sleep 0.5s
+            break
+            ;;
+        "Positions 2")
+            echo "you chose position 2"
+            WAITING_ANIMATION_1=$WAITING_ANIMATION_POSITION2_1
+            WAITING_ANIMATION_2=$WAITING_ANIMATION_POSITION2_2
+            WAITING_ANIMATION_3=$WAITING_ANIMATION_POSITION2_3
+            BITE_ANIMATION_1=$BITE_ANIMATION_POSITION2_1
+            BITE_ANIMATION_2=$BITE_ANIMATION_POSITION2_2
+            BITE_ANIMATION_3=$BITE_ANIMATION_POSITION2_3
+            sleep 0.5s
+            break
+            ;;
+        "Positions 3")
+            echo "you chose position 3"
+            WAITING_ANIMATION_1=$WAITING_ANIMATION_POSITION3_1
+            WAITING_ANIMATION_2=$WAITING_ANIMATION_POSITION3_2
+            WAITING_ANIMATION_3=$WAITING_ANIMATION_POSITION3_3
+            BITE_ANIMATION_1=$BITE_ANIMATION_POSITION3_1
+            BITE_ANIMATION_2=$BITE_ANIMATION_POSITION3_2
+            BITE_ANIMATION_3=$BITE_ANIMATION_POSITION3_3
+            sleep 0.5s
+            break
+            ;;
+        "Quit")
+            exit 0
+            ;;
+        *) echo "invalid option $REPLY"
+            exit 0
+            ;;
+    esac
+done
+}
+
+casting(){
+    clear
+    echo "There is a cast ~"
+    sleep 0.5s
+    clear
+    echo "There is a cast ~~"
+    sleep 0.5s
+
+    clear
+    echo "There is a cast ~~~"
+    sleep 0.5s  
+    clear
+    echo "There is a cast ~~~~o"
+    sleep 0.5s
+    clear
+    echo "There is a cast ~~~~(o)"
+    sleep 0.5s  
+}
 
 # START Animation 
 
-start(){
-CATCH=$RANDOM
-#while :
-#do
-clear
-echo $CATCH!=$RANDOM
-echo -e  $ANIMAT1
-sleep 0.5s
+#initialize borders
+FLOOR=10
+RANGE=100
 
-clear
-echo $CATCH!=$RANDOM
-echo -e  $ANIMAT2
-sleep 0.5s
+#initialize variebles
+CATCH=0
+ENV=0
+WEIGHT=0
 
-clear
-echo $CATCH!=$RANDOM
-echo -e  $ANIMAT3
-sleep 0.5s
+gen_env(){
+    ENV=$RANDOM
+    let "ENV %= $RANGE"  # Ограничение "сверху" числом $RANGE.
+}
 
-clear
-echo $CATCH!=$RANDOM
-echo -e  $ANIMAT2
-sleep 0.5s
+gen_catch(){
+    CATCH=$RANDOM
+    let "CATCH %= $RANGE"  # Ограничение "сверху" числом $RANGE.
+}
 
-if [ $CATCH -eq $RANDOM ]; then
-break 
-clear
-echo "EEEEHHHHHHHHHHHHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
-sleep 10s
-fi
-  
-#done
+gen_weigth(){
+    while [ "$WEIGHT" -le $FLOOR ]
+    do
+        WEIGHT=$RANDOM
+        let "WEIGHT %= $RANGE"  # Ограничение "сверху" числом $RANGE.
+    done
+}
+
+text_catcha(){
+    clear
+    echo "CCCCAAAATTCCHAAAAA!!!!!!"
+    sleep 1s
+}
+
+waiting(){
+gen_env
+    #while :
+    #do
+        clear
+        gen_catch
+        if [ $CATCH == $ENV ]; then 
+            text_catcha
+            break;
+        else
+            #echo $CATCH!=$ENV #DEBUG TAKT INFO  
+            echo -e  $WAITING_ANIMATION_1
+            sleep 0.75s
+        fi
+
+        clear
+        gen_catch
+        if [ $CATCH == $ENV ]; then 
+            text_catcha
+            break;
+        else
+            #echo $CATCH!=$ENV #DEBUG TAKT INFO  
+            echo -e $WAITING_ANIMATION_2
+            sleep 0.75s
+        fi
+
+        clear
+        gen_catch
+        if [ $CATCH == $ENV ]; then 
+            text_catcha
+            break;
+        else
+            #echo $CATCH!=$ENV #DEBUG TAKT INFO  
+            echo -e $WAITING_ANIMATION_3
+            sleep 0.75s
+        fi
+
+        clear
+        gen_catch
+        if [ $CATCH == $ENV ]; then 
+            text_catcha
+            break;
+        else
+            #echo $CATCH!=$ENV #DEBUG TAKT INFO  
+            echo -e $WAITING_ANIMATION_2
+            sleep 0.75s
+        fi
+    #done
+}
 
 # Animation PocKlewKI BLEAT
-# Logic 
-FISH=FALSE
-while [ $FISH == "FALSE" ]
-do
+
+BITE_ANIMATION(){
     clear
-echo "To catch fish press 'Space'"
-    echo -e  $ANIMAT10
-echo $FISH    
-sleep 2s
+        echo -e $BITE_ANIMATION_1   
+    sleep 1s
     clear
-echo "To catch fish press 'Space'"
-    echo -e  $ANIMAT11
-echo $FISH    
-sleep 2s
+        echo -e $BITE_ANIMATION_2  
+    sleep 1s
     clear
-echo "To catch fish press 'Space'"
-    echo -e  $ANIMAT12
-echo $FISH    
-sleep 2s
+        echo -e $BITE_ANIMATION_3  
+    sleep 1s
+    clear   
+        echo -e $BITE_ANIMATION_2
+    sleep 1s
+}
+
+press_F(){
+    echo -ne "\rTo catch fish press 'F' ."
+    sleep 0.2s
+    echo -ne "\rTo catch fish press 'F' .."
+    sleep 0.2s
+    echo -ne "\rTo catch fish press 'F' ..."
+    sleep 0.2s
+    echo -ne "\rTo catch fish press 'F' ...."
+    echo ""
+    sleep 0.2s
+}
+
+skip_bite(){
     clear
-echo "To catch fish press 'Space'"
-    echo -e  $ANIMAT11
-    sleep 2s
-if [[ "$SELECT" != $'\x20' ]]; then 
-  read -s -N 1 SELECT
-  [[ "$SELECT" == $'\x20' ]] && FISH=TRUE
-    break
-fi
+    echo "You are skiped bite (X_X)"
+    sleep 5s
+}
+
+got_off(){ 
+    clear
+    echo "Got off fish :("
+    echo "Let's try again!"
+    echo "Good Luck in next time!"
+    sleep 5s
+}
+
+caught(){
+    clear
+    echo "We are caught! :)"
+    echo -e $(shuf -n1 -e "\e[31mCarpe\e[0m" "\e[32mRuff\e[0m" "\e[33mRoach\e[0m" "\e[34mPike\e[0m")
+    echo "WEIGHT: $WEIGHT g."
+    sleep 5s
+}
+
+result(){
+    if (( RANDOM % 2 )); 
+        then got_off;
+        else caught; 
+    fi
+}
+
+bite(){
+stty -echo
+PROGRESS=0
+BAR='>==============================================<'
+FLOOR=20
+RANGE=80
+number=0   #initialize
+    while [ "$number" -le $FLOOR ]
+    do
+        number=$RANDOM
+        let "number %= $RANGE"  # Ограничение "сверху" числом $RANGE.
+    done 
+   
+BITE_ANIMATION        
+
+press_F
+
+for (( i=1; i <= number; i++ )); do
+read -rsn1 -t 5 input 
+    if [ "$input" = "f" ]; then     
+        ((PROGRESS+=1))
+        let "PROGRESS+=1";
+    else
+        ((PROGRESS-=1))
+        let "PROGRESS-=1";
+    fi
+
+    if [ "$PROGRESS" = "50" ]; then     
+        echo ""
+        echo "What i'd catch ???"
+        sleep 2s
+        result         
+        break;
+    fi
+
+    if [ "$i" = "$number" ]; then
+        if [ "$PROGRESS" -le "0" ]; then
+        skip_bite
+        break;
+    else
+        clear
+        echo "don't have a time"
+        sleep 3s
+        got_off 
+        break;
+        fi
+    fi
+echo -ne "\r${BAR:0:$PROGRESS}" # print $PROGRESS chars of $BAR from 0 position
 done
+echo "" 
+}
 
-
-#Print fishs & witgh
-
-clear
-echo -e " alpha-Fish-001 {250}gr "
+#Notification Restart or Exit 
+quit(){
+stty echo
+clear 
+echo "Restart or Exit "
+echo 'Please enter your choice'
+select opt in restart quit ; 
+do
+     case $opt in
+       "restart")
+            echo "you chose restart" 
+            game
+            ;;
+        "quit")
+            clear
+            echo "Exit..."
+            exit 0 
+            ;;
+        *) echo invalid option ;;
+    esac
+done
 sleep 5s
 }
 
-start 
-#Notification Restart or Exit 
+#Inter on the game (like psvm in java)
+game(){
+    select_position
+    casting
+    waiting
+    bite
+    quit
+}
 
-clear 
-echo -e "Restart or Exit "
-sleep 3s
-# if [select 1]; then
-# goto start()
-# fi
-
-# if [secelct 2]; then 
-clear
-echo "Exit..."
-exit 0
-# fi
+game #up method gamet
 
